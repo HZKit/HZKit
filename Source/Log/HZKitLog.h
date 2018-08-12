8afc13c09d39129e63fadedeb7d6671b50017656
Hertz Wang <hertz@hertzwang.com>
2018-08-12 21:45:22 +0800

    update

---
+++ Source/Log/HZKitLog.h
@@ -0,0 +1,14 @@
+//
+//  HZKitLog.h
+//  HZKit
+//
+//  Created by Hertz Wang on 2018/8/12.
+//  Copyright © 2018 Hertz Wang. All rights reserved.
+//
+
+#ifndef HZKitLog_h
+#define HZKitLog_h
+
+// QLog 官方日志 https://developer.apple.com/library/content/samplecode/MVCNetworking/Introduction/Intro.html#//apple_ref/doc/uid/DTS40010443-Intro-DontLinkElementID_2
+
+#endif /* HZKitLog_h */
