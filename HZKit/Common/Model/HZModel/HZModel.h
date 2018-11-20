068a3ef3e430d4f8fdaa2fcf61256db082fcc92a
Haizhou Wang <haizhou.wang@dnurse.cn>
2018-11-20 17:14:19 +0800

    add HZModel

---
+++ HZKit/Common/Model/HZModel/HZModel.h
@@ -0,0 +1,28 @@
+//
+//  HZModel.h
+//  HZKit
+//
+//  Created by HertzWang on 2018/11/20.
+//  Copyright © 2018年 Hertz Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HZModel : NSObject<NSCoding, NSCopying>
+
+@end
+
+
+@interface HZModel (JSON)
+
+
+
+@end
+
+@interface HZModel (Database)
+
+@end
+
+NS_ASSUME_NONNULL_END
