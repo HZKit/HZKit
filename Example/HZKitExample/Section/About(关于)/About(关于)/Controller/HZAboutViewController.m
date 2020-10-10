6deb7f015c75b69c841e002d1ab3d29c3062d2d2
haizhou.wang <haizhou.wang@wm-motor.com>
2020-10-10 16:20:47 +0800

    Sign in with Apple

--- Example/HZKitExample/Section/About(关于)/About(关于)/Controller/HZAboutViewController.m
+++ Example/HZKitExample/Section/About(关于)/About(关于)/Controller/HZAboutViewController.m
@@ -8,7 +8,11 @@
 
 #import "HZAboutViewController.h"
 
-@interface HZAboutViewController ()
+#import <AuthenticationServices/AuthenticationServices.h>
+
+@interface HZAboutViewController () <ASAuthorizationControllerDelegate>
+
+@property (nonatomic, strong) ASAuthorizationAppleIDButton *appleIDButton;
 
 @end
 
@@ -18,10 +22,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [self initView];
+    [self _initView];
 }
 
-- (void)initView {
+#pragma mark - Private
+
+- (void)_initView {
     self.title = @"关于";
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
     
@@ -51,6 +57,52 @@
     version.textAlignment = NSTextAlignmentCenter;
     version.textColor = [UIColor grayColor];
     [self.view addSubview:version];
+    
+    // 通过Apple登录 https://developer.apple.com/design/human-interface-guidelines/sign-in-with-apple/overview/buttons/
+    if (@available(iOS 13.0, *)) {
+        self.appleIDButton.frame = CGRectMake((screenWidth - 140) * 0.5, screenHeight - 60 - self.tabBarController.tabBar.bounds.size.height, 140, 30);
+        [self.view addSubview:self.appleIDButton];
+    }
+}
+
+/// 通过Apple登录
+- (void)_appleIdSignAction API_AVAILABLE(ios(13.0)) {
+    ASAuthorizationAppleIDProvider *provider = [[ASAuthorizationAppleIDProvider alloc] init];
+    ASAuthorizationAppleIDRequest *request = provider.createRequest;
+    request.requestedScopes = @[ASAuthorizationScopeEmail, ASAuthorizationScopeFullName];
+    ASAuthorizationController *controller = [[ASAuthorizationController alloc] initWithAuthorizationRequests:@[request]];
+    controller.delegate = self;
+    [controller performRequests];
+}
+
+#pragma mark - ASAuthorizationControllerDelegate
+
+- (void)authorizationController:(ASAuthorizationController *)controller didCompleteWithAuthorization:(ASAuthorization *)authorization API_AVAILABLE(ios(13.0)){
+#warning 待验证
+    NSString *userName = nil;
+    if ([authorization.credential isKindOfClass:[ASAuthorizationAppleIDCredential class]]) {
+        ASAuthorizationAppleIDCredential *credential = (ASAuthorizationAppleIDCredential *)authorization.credential;
+        userName = credential.email;
+    } else if ([authorization.credential isKindOfClass:[ASPasswordCredential class]]) {
+        ASPasswordCredential *credential = (ASPasswordCredential *)authorization.credential;
+        userName = credential.user;
+    }
+    self.appleIDButton.hidden = (userName != nil);
+}
+
+/// 取消
+- (void)authorizationController:(ASAuthorizationController *)controller didCompleteWithError:(NSError *)error API_AVAILABLE(ios(13.0)) {
+    NSLog(@"");
+}
+
+#pragma mark - Lazy load
+
+- (ASAuthorizationAppleIDButton *)appleIDButton API_AVAILABLE(ios(13.0)) {
+    if (_appleIDButton == nil) {
+        _appleIDButton = [ASAuthorizationAppleIDButton buttonWithType:ASAuthorizationAppleIDButtonTypeSignIn style:ASAuthorizationAppleIDButtonStyleBlack];
+        [_appleIDButton addTarget:self action:@selector(_appleIdSignAction) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _appleIDButton;
 }
 
 @end
